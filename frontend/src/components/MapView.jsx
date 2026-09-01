0e1e104173815e00b1ad72cd428e20157570ba66
Yasar-17 <mohammedyasar990@gmail.com>
2026-09-01 23:16:06 +0530

    Switch map tiles from CARTO (now requires API key) to OpenStreetMap

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -48,8 +48,8 @@ export default function MapView({ detections, selectedId, onSelect,
       >
         <AutoResize />
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         {(detections ?? []).map((d) => {
           const selected = d.id === selectedId
